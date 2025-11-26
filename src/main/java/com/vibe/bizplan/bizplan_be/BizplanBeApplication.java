a54443d49e7373bd59ee0f7bec40bdfe4bdbf094
voom20 <voom20@gmail.com>
2025-11-26 21:22:40 +0900

    Initial commit

---
+++ src/main/java/com/vibe/bizplan/bizplan_be/BizplanBeApplication.java
@@ -0,0 +1,13 @@
+package com.vibe.bizplan.bizplan_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizplanBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizplanBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vibe/bizplan/bizplan_be/BizplanBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibe.bizplan.bizplan_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizplanBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
