7b54450012abaed0338dbaa1f5978060dad5d5c6
voom20 <voom20@gmail.com>
2025-12-06 15:07:40 +0900

    chore(test): remove unused Hamcrest Matchers import
    - Remove unused static import 'org.hamcrest.Matchers.*' from WizardControllerIntegrationTest
    - Resolves IDE warning (code 268435844)

--- src/test/java/com/vibe/bizplan/bizplan_be/api/WizardControllerIntegrationTest.java
+++ src/test/java/com/vibe/bizplan/bizplan_be/api/WizardControllerIntegrationTest.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
